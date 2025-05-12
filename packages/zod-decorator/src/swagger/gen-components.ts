1cafe9c5a55edbb66d9eb1f8a265c8f27c4cf1e4
NguyenHoangKhang <hoangkhang12789@gmail.com>
2025-05-12 15:30:34 +0700

    fix: gen enum for nested object

--- packages/zod-decorator/src/swagger/gen-components.ts
+++ packages/zod-decorator/src/swagger/gen-components.ts
@@ -1,5 +1,5 @@
 import { createSchema, } from "zod-openapi";
-import { ZodType } from "zod";
+import { ZodObject, ZodType } from "zod";
 
 export function genSchemaComponents(schemas: Record<string, any>): object {
     let Schemas = {};
@@ -12,35 +12,11 @@ export function genSchemaComponents(schemas: Record<string, any>): object {
         if (!isZodType(itemSchema)) {
             continue;
         }
-        let baseSchema = itemSchema;
-        if (baseSchema._def.typeName === 'ZodEffects') {
-            baseSchema = baseSchema._def.schema;
-        }
 
-        if (baseSchema._def.typeName === 'ZodObject' && typeof baseSchema._def.shape === 'function') {
-            const shape = baseSchema._def.shape();
-
-            for (const [fieldName, fieldSchema] of Object.entries(shape)) {
-                if ((fieldSchema as any)._def.typeName === 'ZodEffects' && (fieldSchema as any)._def.schema) {
-                    const schema = (fieldSchema as any)._def.schema;
-                    if (schema._def.innerType && schema._def.innerType._def?.typeName === 'ZodNativeEnum') {
-                        const ref = (fieldSchema as any)._def.zodOpenApi?.openapi?.ref;
-                        if (ref) {
-                            const value = Object.fromEntries(
-                                Object.entries(schema._def.innerType._def.values).filter(([key, val]) => isNaN(Number(key)) && typeof val === 'number')
-                            );
-                            const enumComponents = generateNativeEnumComponents(ref, value);
-                            if (enumComponents) {
-                                Schemas = {
-                                    ...Schemas,
-                                    ...enumComponents
-                                };
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        Schemas = {
+            ...Schemas,
+            ...genEnumSchema(itemSchema)
+        };
 
         let { components } = createSchema(itemSchema as ZodType, {
             schemaType: 'input',
@@ -75,14 +51,6 @@ function isZodType(obj: any): boolean {
     );
 }
 
-function isNativeEnum(obj: any): boolean {
-    if (!obj || typeof obj !== 'object') {
-        return false;
-    }
-    const keys = Object.keys(obj);
-    if (keys.length === 0) return false;
-    return keys.some(key => !isNaN(Number(key)));
-}
 
 function generateNativeEnumComponents(enumName: string, enumObj: any): object {
     try {
@@ -123,3 +91,105 @@ function generateNativeEnumComponents(enumName: string, enumObj: any): object {
         return {};
     }
 }
+
+function genEnumSchema(itemSchema) {
+    let Schemas = {};
+    try {
+        const shape = itemSchema._def.shape();
+
+
+        for (const [fieldName, fieldSchema] of Object.entries(shape)) {
+            if ((fieldSchema as any)._def.typeName === 'ZodOptional') {
+                if ((fieldSchema as any)._def.innerType && (fieldSchema as any)._def.innerType._def.innerType instanceof ZodObject && fieldName !== 'error') {
+                    const item = (fieldSchema as any)._def.innerType._def.innerType;
+                    Schemas = {
+                        ...Schemas,
+                        ...genEnumSchema(item)
+                    };
+                }
+
+                if ((fieldSchema as any)._def.innerType instanceof ZodObject) {
+                    const item = (fieldSchema as any)._def.innerType;
+                    Schemas = {
+                        ...Schemas,
+                        ...genEnumSchema(item)
+                    }
+                }
+
+            }
+
+            if (fieldSchema instanceof ZodObject) {
+                Schemas = {
+                    ...Schemas,
+                    ...genEnumSchema(fieldSchema)
+                };
+            }
+            if ((fieldSchema as any)._def.innerType && (fieldSchema as any)._def.innerType._def?.typeName === 'ZodNativeEnum') {
+
+                const ref = (fieldSchema as any)._def.zodOpenApi.openapi.ref;
+                const value = Object.fromEntries(
+                    Object.entries((fieldSchema as any)._def.innerType._def.values).filter(([key, val]) => isNaN(Number(key)) && typeof val === 'number')
+                );
+                const enumComponents = generateNativeEnumComponents(ref, value)
+                if (enumComponents) {
+                    Schemas = {
+                        ...Schemas,
+                        ...enumComponents
+                    };
+                }
+            } else if ((fieldSchema as any)._def.values) {
+                const ref = (fieldSchema as any)._def.zodOpenApi.openapi.ref;
+                const value = Object.fromEntries(
+                    Object.entries((fieldSchema as any)._def.values).filter(([key, val]) => isNaN(Number(key)) && typeof val === 'number')
+                );
+                const enumComponents = generateNativeEnumComponents(ref, value)
+                if (enumComponents) {
+                    Schemas = {
+                        ...Schemas,
+                        ...enumComponents
+                    };
+                }
+
+            }
+            else if ((fieldSchema as any)._def.typeName === 'ZodEffects') {
+
+                const schema = (fieldSchema as any)._def.schema
+                if (schema._def.innerType && schema._def.innerType._def?.typeName === 'ZodNativeEnum') {
+                    const ref = (fieldSchema as any)._def.zodOpenApi.openapi.ref;
+                    const value = Object.fromEntries(
+                        Object.entries(schema._def.innerType._def.values).filter(([key, val]) => isNaN(Number(key)) && typeof val === 'number')
+                    );
+                    const enumComponents = generateNativeEnumComponents(ref, value)
+                    if (enumComponents) {
+                        Schemas = {
+                            ...Schemas,
+                            ...enumComponents
+                        };
+                    }
+                }
+
+            } else if ((fieldSchema as any)._def.typeName === 'ZodOptional') {
+
+                if ((fieldSchema as any)._def.innerType._def.schema && (fieldSchema as any)._def.innerType._def.schema._def.typeName === 'ZodNativeEnum') {
+                    const ref = (fieldSchema as any)._def.zodOpenApi.openapi.ref;
+                    const value = Object.fromEntries(
+                        Object.entries((fieldSchema as any)._def.innerType._def.schema._def.values).filter(([key, val]) => isNaN(Number(key)) && typeof val === 'number')
+                    );
+                    const enumComponents = generateNativeEnumComponents(ref, value)
+                    if (enumComponents) {
+                        Schemas = {
+                            ...Schemas,
+                            ...enumComponents
+                        };
+                    }
+
+                }
+
+            }
+        }
+        return Schemas
+    } catch {
+        return
+    }
+}
+
