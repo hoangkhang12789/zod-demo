4f6dc5ec35da7804bace10fe1f681e3fc4bd6bd7
Quang Thai <tranquangthai.contact@gmail.com>
2025-05-13 15:35:53 +0700

    fix: pagination dto type

--- apps/zod-demo/src/dtos/pagination.dto.ts
+++ apps/zod-demo/src/dtos/pagination.dto.ts
@@ -3,24 +3,30 @@ import { z } from '@packages/zod-decorator';
 export const BasePaginationDto = z.object({
   limit: z.coerce.number().nullable().optional(),
   nextPageToken: z.string().optional(),
-  prevPageToken: z.string().optional()
+  prevPageToken: z.string().optional(),
 });
 
-export const paginationRefinement: Parameters<typeof BasePaginationDto['refine']> = [
-  (data) => !(data.nextPageToken && data.prevPageToken),
+export const paginationRefinement: Parameters<
+  (typeof BasePaginationDto)['refine']
+> = [
+  (data: z.infer<typeof BasePaginationDto>): boolean => {
+    return !(data.nextPageToken && data.prevPageToken);
+  },
   {
-    message: 'Only one of nextPageToken or prevPageToken may be provided'
-  }
+    message: 'Only one of nextPageToken or prevPageToken may be provided',
+  },
 ];
 
-export const PaginationDto = BasePaginationDto.refine(...paginationRefinement).openapi({
+export const PaginationDto = BasePaginationDto.refine(
+  ...paginationRefinement,
+).openapi({
   ref: 'PaginationDto',
   oneOf: [
     {
-      required: ['nextPageToken']
+      required: ['nextPageToken'],
     },
     {
-      required: ['prevPageToken']
-    }
-  ]
+      required: ['prevPageToken'],
+    },
+  ],
 });
